b64761da20f681167a39db6d6f568d9ec7b7090d
nowLetsgo <lipeihua0922@163.com>
2021-08-21 11:55:40 +0800

    day10-上午

---
+++ day10-node05/01.MIME类型/MIME类型.js
@@ -0,0 +1,16 @@
+/* 
+    Content-Type用于指示资源的MIME类型，告诉客户端实际上服务端返回的内容的类型
+
+    MIME类型也叫作媒体类型，表示文档文件的内容格式
+
+    常见的MIME类型：
+        text/plain: 普通文本
+        text/css: css文件
+        text/html: html文件
+        application/javascript: js文件
+        application/x-www-form-urlencoded: form表单类型
+        image/图片格式: 图片
+        video/视频格式: 视频
+        audio/音频格式: 音频
+
+*/
---
+++ day10-node05/01.MIME类型/server2.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs")
+//获取文件路径
+const pathFile = path.resolve(__dirname, "./05.jpg");
+
+const server = http.createServer((req, res) => {
+    console.log("有请求进来了");
+
+    //把要返回的文件当做一个可读流
+    const rs = fs.createReadStream(pathFile);
+
+    //因为响应的是一个html文件，所以要设置MIME类型
+    res.setHeader("Content-type", "image/jpg;charset=utf-8")
+
+    //响应res本身也是一个可写流，所以可以直接把可读流写入到res中
+    rs.pipe(res)
+})
+
+server.listen(3000, "127.0.0.1", (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log("服务器启动成功 http://127.0.0.1:3000");
+})
