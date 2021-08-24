ddadd1888c209a68b35f3653fe84993f0810296a
nowLetsgo <lipeihua0922@163.com>
2021-08-24 16:54:38 +0800

    day12-over

--- day12-axios/06.取消请求/server.js
+++ day12-axios/06.取消请求/server.js
@@ -8,20 +8,16 @@ app.get("/login", (req, res) => {
     console.log(req.query);
     //允许某一个地址进行跨域(设置响应头的Access-Control-Allow-Origin属性值为跨域的地址)
     res.set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
-    res.send({
-        code: 10001,
-        msg: "密码错误"
-    })
+
+    //延迟5s返回响应
+    setTimeout(() => {
+        res.send({
+            code: 10001,
+            msg: "密码错误"
+        })
+    }, 5000)
 })
 
-app.get("/regist", (req, res) => {
-    console.log(req.query);
-    //允许某一个地址进行跨域(设置响应头的Access-Control-Allow-Origin属性值为跨域的地址)
-    res.set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
-    res.send({
-        type: "注册成功"
-    })
-})
 
 
 app.listen("3004", (err) => {
