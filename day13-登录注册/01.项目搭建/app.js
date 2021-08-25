0c3fb60280cbd31358a55eaeee11c2d9e6b74270
nowLetsgo <lipeihua0922@163.com>
2021-08-25 14:30:59 +0800

    day13-登录

--- day13-登录注册/01.项目搭建/app.js
+++ day13-登录注册/01.项目搭建/app.js
@@ -62,6 +62,44 @@ app.post("/register", async (req, res) => {
     })
 })
 
+// 登录接口
+app.post("/login", async (req, res) => {
+    console.log("登录的数据", req.body);
+    //拿到用户的登录信息
+    const {
+        username,
+        password
+    } = req.body;
+
+    //判断用户名是否存在
+    const isHasUser = await userModel.findOne({
+        username
+    })
+    console.log(isHasUser)
+
+    //当用户名不存在的时候 则直接响应信息
+    if (!isHasUser) {
+        return res.send({
+            code: 10001,
+            msg: "用户名不存在"
+        })
+    }
+
+    //如果用户名存在，则判断密码是否正确
+    if (isHasUser.password !== password) {
+        return res.send({
+            code: 10002,
+            msg: "密码错误"
+        })
+    }
+
+    //当代码运行到这个位置，则表示登录成功
+    res.send({
+        code: 10000,
+        msg: "登录成功"
+    })
+})
+
 app.listen("3005", err => {
     if (err) {
         console.log(err);
