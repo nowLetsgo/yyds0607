62ae3fcdce0e218eafef500890bfcc698a14b8c4
nowLetsgo <lipeihua0922@163.com>
2021-08-25 11:48:23 +0800

    day13-注册

--- day13-登录注册/01.项目搭建/app.js
+++ day13-登录注册/01.项目搭建/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+//连接数据库
+require("./db");
+//引入userModel集合
+const userModel = require("./model/userModel")
+
 const path = require("path");
 
 //官方的静态资源中间件
@@ -16,13 +21,44 @@ app.use(express.urlencoded({ //解决post发送的报文体格式是表单格式
 app.use(express.json()); //解决post发送的报文体格式是json格式
 
 //注册接口
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     //查看用户发送的信息
     console.log("注册时用户发送的数据", req.body);
 
+    //获取到用户发送信息的各个字段
+    const {
+        username,
+        password
+    } = req.body;
+
+    //判断当前的用户名是否被注册，去数据库进行查询
+    //数据库的查询是一个异步的 返回promise对象
+    const isHasUser = await userModel.findOne({
+        username
+    })
+
+    //查询不到则为null 如果查询到则为查询的到json对象
+    console.log(isHasUser);
+
+    //判断如果查询到了 则应该响应一个信息 用户名被注册
+    if (isHasUser) {
+        return res.send({
+            code: 10001,
+            msg: "用户名已经被注册"
+        })
+    }
+    //如果查询不到，则在数据库添加信息
+    //数据库的操作是异步的，所以还要进行异步处理
+    const registerData = await userModel.create({
+        username,
+        password
+    })
+
+    console.log(registerData)
+
     res.send({
         code: 10000,
-        msg: "登录成功"
+        msg: "注册成功"
     })
 })
 
