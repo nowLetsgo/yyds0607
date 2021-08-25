e2772807d7984c8f3b341855315a176bb18b6da1
nowLetsgo <lipeihua0922@163.com>
2021-08-25 10:44:50 +0800

    day13-搭建

--- day13-登录注册/01.项目搭建/app.js
+++ day13-登录注册/01.项目搭建/app.js
@@ -9,6 +9,22 @@ const path = require("path");
 app.use("/public/", express.static(path.resolve(__dirname, "./public")))
 app.use("", express.static(path.resolve(__dirname, "./views")))
 
+//解决post请求的报文体body的数据 中间件
+app.use(express.urlencoded({ //解决post发送的报文体格式是表单格式
+    extended: false
+}))
+app.use(express.json()); //解决post发送的报文体格式是json格式
+
+//注册接口
+app.post("/register", (req, res) => {
+    //查看用户发送的信息
+    console.log("注册时用户发送的数据", req.body);
+
+    res.send({
+        code: 10000,
+        msg: "登录成功"
+    })
+})
 
 app.listen("3005", err => {
     if (err) {
