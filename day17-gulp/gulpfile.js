126e931c48dd960a5f1e2e4c6865cc7551f4d9ed
nowLetsgo <lipeihua0922@163.com>
2021-08-31 10:42:29 +0800

    day17-gulp

--- day17-gulp/gulpfile.js
+++ day17-gulp/gulpfile.js
@@ -2,6 +2,8 @@
 const gulp = require("gulp");
 //语法检查的jshint的包
 const jshint = require('gulp-jshint')
+//babel编译的包
+const babel = require("gulp-babel");
 
 //新建一个任务
 gulp.task("jshint", () => {
@@ -12,4 +14,15 @@ gulp.task("jshint", () => {
             asi: false //可以不添加分号
         }))
         .pipe(jshint.reporter("default"))
+})
+
+//新建一个babel的任务
+gulp.task("babel", () => {
+    return gulp.src("./src/js/*.js")
+        .pipe(babel({
+            //使用babel进行编译
+            //预设:把es6代码编译为es5，将模块化编译成commonjs
+            presets: ["@babel/env"]
+        }))
+        .pipe(gulp.dest("./dist/js/"))
 })
